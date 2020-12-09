66d7af0a1d7f08ee7864114a08f6a7f72f0a322d
Marcos <m_paulo2000@hotmail.com>
2020-12-09 16:12:41 -0300

    Add files via upload

---
+++ Atividade POO/Questao5/Exemplo.java
@@ -0,0 +1,12 @@
+package AtividadePoo;
+
+public class Exemplo {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 1;
+        int soma = a +b;
+        System.out.println("Soma: " + soma);
+    }
+}
+
